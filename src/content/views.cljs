330e0a4ecdefc6506c1e8282ad975ba838b5e20d
Alex Dik <mail@alexdik.de>
2022-01-19 16:54:04 +0100

    feat: add basic grid layout

--- src/content/views.cljs
+++ src/content/views.cljs
@@ -1,14 +1,24 @@
 (ns content.views
-  (:require ["@mui/material/Box" :default Box]
+  (:require ["@mui/material/Container" :default Container]
+            ["@mui/material/Grid" :default Grid]
             [content.subs :as subs]
             [re-frame.core :as rf]
             ["react-markdown" :default ReactMarkdown]))
+
+(defn layout [children]
+  [:> Container {:maxWidth "md"}
+   [:> Grid {:container true :spacing {:xs 2}}
+    [:> Grid {:item true :xs 1}
+     [:img {:src "img/bunnies.svg" :alt "🐇🐇" :width "100%"}]]
+    [:> Grid {:item true :xs 11} "person"]
+    [:> Grid {:item true :xs 1} "overview"]
+    [:> Grid {:item true :xs 11} "content" children]]])
 
 (defn markdown [md]
   [:> ReactMarkdown md])
 
 (defn content-renderer []
   (let [elements @(rf/subscribe [::subs/elements])]
-    [:> Box "content: "
+    [layout
      (for [{id :id md :markdown} elements]
        ^{:key id} [markdown md])]))
