8569675bf2e74f30f65955fd9910a47b53b9782d
Alex Dik <mail@alexdik.de>
2022-02-01 00:25:36 +0100

    feat: add top nav toolbar style

--- src/app/views.cljs
+++ src/app/views.cljs
@@ -3,12 +3,16 @@
             ["@mui/material/Container" :default Container]
             ["@mui/material/Grid" :default Grid]
             ["@mui/material/Skeleton" :default Skeleton]
+            ["@mui/material/Toolbar" :default Toolbar]
+            ["@mui/material/Link" :default Link]
+            ["@mui/material/Button" :default Button]
             ["@mui/material/Typography" :default Typography]
             [app.components.markdown :as md]
             [app.subs :as subs]
             [re-frame.core :as rf]
             [reagent.core :as r]
-            ["react-router-dom" :refer (HashRouter Routes Route Link)]))
+            ["react-router-dom" :refer (HashRouter Routes Route Link)
+             :rename {Link RouteLink}]))
 
 (defn md-elements [elements]
   [:<>
@@ -62,8 +66,15 @@
 (defn navigation []
   (let [items @(rf/subscribe [::subs/navigation])]
     [:> AppBar {:position "static"}
-     (for [{:keys [id path nav]} items]
-       ^{:key id} [:> Link {:to path} nav])]))
+     [:> Toolbar
+      (for [{:keys [id path nav]} items]
+        ^{:key id} [:> Button
+                    [:> Link
+                     {:underline "none"
+                      :color "white"
+                      :component RouteLink
+                      :to path}
+                     nav]])]]))
 
 (defn page []
   [:> Container {:maxWidth "lg"}
