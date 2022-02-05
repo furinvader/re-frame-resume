4943cfdea042a3be213e625572cf721ab8d2cf9a
Alex Dik <mail@alexdik.de>
2022-02-05 06:49:29 +0100

    refactor: clean up app views

--- src/app/views.cljs
+++ src/app/views.cljs
@@ -15,86 +15,85 @@
              :rename {Link RouteLink}]
             [reagent.core :as r]))
 
+(defn navigation [pages]
+  [:> AppBar {:position "static"}
+   [:> Toolbar
+    (for [{:keys [id path title nav]} pages]
+      [:> Button {:key id}
+       [:> Link
+        {:underline "none"
+         :color "white"
+         :component RouteLink
+         :to path}
+        (if (empty? nav) title nav)]])]])
+
+(defn image []
+  [:div
+   {:style {:width "100%"
+            :height "100%"
+            :min-height 100}
+    :class "bunnify"}])
+
 (defn md-elements [elements]
   [:<>
    (for [{:keys [id text]} elements]
      ^{:key id} [md/mui text])])
 
-(defn image []
-  [:div
-   {:style {:width "100%"
-            :height "100%"}
-    :class "bunnify"}])
+(defn header [contents]
+  [md-elements contents])
 
-(defn header []
-  (let [elements @(rf/subscribe [::subs/contents-by-position "header"])
-        loading? (empty? elements)]
-    (if-not loading?
-      [md-elements elements]
-      [:<>
-       [:> Typography {:variant "h1"} [:> Skeleton {:width "40%"}]]
-       [:> Typography {:variant "h2"} [:> Skeleton {:width "50%"}]]])))
+(defn side [contents]
+  [md-elements contents])
 
-(defn side []
-  (let [elements @(rf/subscribe [::subs/contents-by-position "side"])
-        loading? (empty? elements)]
-    (if-not loading?
-      [md-elements elements]
-      [:<>
-       [:> Skeleton {:variant "rectangular" :height 100}]])))
+(defn main [contents]
+  [md-elements contents])
 
-(defn main []
-  (let [elements @(rf/subscribe [::subs/contents-by-position "main"])
-        loading? (empty? elements)]
-    (if-not loading?
-      [md-elements elements]
-      [:<>
-       [:> Typography {:variant "h1"} [:> Skeleton {:width "50%"}]]
-       [:> Typography {:variant "body1"} [:> Skeleton]]
-       [:> Typography {:variant "body1"} [:> Skeleton]]
-       [:> Typography {:variant "body1"} [:> Skeleton]]
-       [:> Typography {:variant "body1"} [:> Skeleton]]
-       [:> Typography {:variant "body1"} [:> Skeleton]]
-       [:> Typography {:variant "body1"} [:> Skeleton {:width "30%"}]]])))
+(defn footer [contents]
+  [md-elements contents])
 
-(defn footer []
-  (let [elements @(rf/subscribe [::subs/contents-by-position "footer"])
-        loading? (empty? elements)]
-    (if-not loading?
-      [md-elements elements]
-      [:> Typography {:variant "body2"} [:> Skeleton]])))
+(defn side-preview []
+  [:> Skeleton {:variant "rectangular" :height 100}])
 
-(defn navigation []
-  (let [items @(rf/subscribe [::subs/navigation])]
-    [:> AppBar {:position "static"}
-     [:> Toolbar
-      (for [{:keys [id path title nav]} items]
-        ^{:key id} [:> Button
-                    [:> Link
-                     {:underline "none"
-                      :color "white"
-                      :component RouteLink
-                      :to path}
-                     (if (empty? nav) title nav)]])]]))
+(defn header-preview []
+  [:<>
+   [:> Typography {:variant "h1"} [:> Skeleton {:width "40%"}]]
+   [:> Typography {:variant "h2"} [:> Skeleton {:width "50%"}]]])
 
-(defn app-page []
-  [:> Grid {:container true :spacing {:xs 2}}
-   [:> Grid {:item true :xs 1} [image]]
-   [:> Grid {:item true :xs 11} [header]]
-   [:> Grid {:item true :xs 1} [side]]
-   [:> Grid {:item true :xs 11} [main]]
-   [:> Grid {:item true :xs 12} [footer]]])
+(defn main-preview []
+  [:<>
+   [:> Typography {:variant "h1"} [:> Skeleton {:width "50%"}]]
+   [:> Typography {:variant "body1"} [:> Skeleton]]
+   [:> Typography {:variant "body1"} [:> Skeleton]]
+   [:> Typography {:variant "body1"} [:> Skeleton]]
+   [:> Typography {:variant "body1"} [:> Skeleton]]
+   [:> Typography {:variant "body1"} [:> Skeleton]]
+   [:> Typography {:variant "body1"} [:> Skeleton {:width "30%"}]]])
 
-(defn app-routes []
-  [:> Routes
-   [:> Route {:path "*"
-              :element (r/as-element [routing/path-changed [app-page]])}]])
+(defn footer-preview []
+  [:> Typography {:variant "body2"} [:> Skeleton]])
 
-(defn app-base []
-  [:> Container {:maxWidth "lg"}
-   [navigation]
-   [app-routes]])
+(defn app-route []
+  (let [pages @(rf/subscribe [::subs/navigation])
+        loading? (empty? pages)]
+    [:> Container {:maxWidth "lg"}
+     [navigation pages]
+     [:> Grid {:container true :spacing {:xs 2}}
+      [:> Grid {:item true :xs 1} [image]]
+      [:> Grid {:item true :xs 11}
+       [header @(rf/subscribe [::subs/contents-by-position "header"])
+        (when loading? [header-preview])]]
+      [:> Grid {:item true :xs 1}
+       [side @(rf/subscribe [::subs/contents-by-position "side"])]
+       (when loading? [side-preview])]
+      [:> Grid {:item true :xs 11}
+       [main @(rf/subscribe [::subs/contents-by-position "main"])]
+       (when loading? [main-preview])]
+      [:> Grid {:item true :xs 12}
+       [footer @(rf/subscribe [::subs/contents-by-position "footer"])]
+       (when loading? [footer-preview])]]]))
 
 (defn app []
   [:> BrowserRouter
-   [app-base]])
+   [:> Routes
+    [:> Route {:path "*"
+               :element (r/as-element [routing/path-changed [app-route]])}]]])
