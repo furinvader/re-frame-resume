d7ccdf28f8334d3f68b6c866d3f0fdee58271c8c
Alex Dik <mail@alexdik.de>
2022-02-06 01:00:09 +0100

    refactor: load current page contents

--- src/app/events.cljs
+++ src/app/events.cljs
@@ -1,12 +1,16 @@
 (ns app.events
-  (:require [app.db :as db]
+  (:require [app.api.events :as api]
+            [app.db :as db]
             [app.fx :as fx]
             [day8.re-frame.tracing :refer-macros [fn-traced]]
             [re-frame.core :as rf]))
 
-(rf/reg-event-db
+(rf/reg-event-fx
  ::initialize
- (fn-traced [] db/default-db))
+ (fn-traced
+  []
+  {:db db/default-db
+   :fx [[:dispatch [::api/init-pages]]]}))
 
 (rf/reg-event-fx
  ::view-ready
--- src/app/routing/events.cljs
+++ src/app/routing/events.cljs
@@ -1,6 +1,5 @@
 (ns app.routing.events
-  (:require [app.entities.events :as entities]
-            [app.http.events :as http]
+  (:require [app.api.events :as api]
             [app.routing.db :as db]
             [day8.re-frame.tracing :refer-macros [fn-traced]]
             [re-frame.core :as rf]))
@@ -10,5 +9,4 @@
  (fn-traced
   [{:keys [db]} [_ path]]
   {:db (assoc db ::db/path path)
-   :fx [[:dispatch [::http/request [:get "pages"]
-                    [[::entities/add :pages]]]]]}))
+   :fx [[:dispatch [::api/get-page {:path path}]]]}))
