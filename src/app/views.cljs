96676e7adba0b74f65f9ef1bdbb42e4efce9a083
Alex Dik <mail@alexdik.de>
2022-02-18 23:04:52 +0100

    refactor: clean up app views

--- src/app/views.cljs
+++ src/app/views.cljs
@@ -1,5 +1,6 @@
 (ns app.views
   (:require ["@mui/material/AppBar" :default AppBar]
+            ["@mui/material/Box" :default Box]
             ["@mui/material/Container" :default Container]
             ["@mui/material/Grid" :default Grid]
             ["@mui/material/Skeleton" :default Skeleton]
@@ -22,57 +23,54 @@
    (for [{:keys [id text]} elements]
      ^{:key id} [md/mui text])])
 
-(defn header-preview []
-  [:<>
-   [:> Typography {:variant "h1"} [:> Skeleton {:width "40%"}]]
-   [:> Typography {:variant "h2"} [:> Skeleton {:width "50%"}]]])
-
 (defn header []
   [:<>
    [md-elements @(rf/subscribe [::subs/contents-by-position "header"])]
-   (when @(rf/subscribe [::subs/loading?]) [header-preview])])
-
-(defn main-preview []
-  [:<>
-   [:> Typography {:variant "h1"} [:> Skeleton {:width "50%"}]]
-   [:> Typography {:variant "body1"} [:> Skeleton]]
-   [:> Typography {:variant "body1"} [:> Skeleton]]
-   [:> Typography {:variant "body1"} [:> Skeleton]]
-   [:> Typography {:variant "body1"} [:> Skeleton]]
-   [:> Typography {:variant "body1"} [:> Skeleton]]
-   [:> Typography {:variant "body1"} [:> Skeleton {:width "30%"}]]])
+   (when @(rf/subscribe [::subs/loading?])
+     [:<>
+      [:> Typography {:variant "h1"} [:> Skeleton {:width "40%"}]]
+      [:> Typography {:variant "h2"} [:> Skeleton {:width "50%"}]]])])
 
 (defn main []
   [:<>
    [md-elements @(rf/subscribe [::subs/contents-by-position "main"])]
-   (when @(rf/subscribe [::subs/loading?]) [main-preview])])
-
-(defn side-preview []
-  [:> Skeleton {:variant "rectangular" :height 100}])
+   (when @(rf/subscribe [::subs/loading?])
+     [:<>
+      [:> Typography {:variant "h1"} [:> Skeleton {:width "50%"}]]
+      [:> Typography {:variant "body1"} [:> Skeleton]]
+      [:> Typography {:variant "body1"} [:> Skeleton]]
+      [:> Typography {:variant "body1"} [:> Skeleton]]
+      [:> Typography {:variant "body1"} [:> Skeleton]]
+      [:> Typography {:variant "body1"} [:> Skeleton]]
+      [:> Typography {:variant "body1"} [:> Skeleton {:width "30%"}]]])])
 
 (defn side []
   [:<>
    [md-elements @(rf/subscribe [::subs/contents-by-position "side"])]
-   (when @(rf/subscribe [::subs/loading?]) [side-preview])])
-
-(defn footer-preview []
-  [:> Typography {:variant "body2"} [:> Skeleton]])
+   (when @(rf/subscribe [::subs/loading?])
+     [:> Skeleton {:variant "rectangular" :height 100}])])
 
 (defn footer []
   [:<>
    [md-elements @(rf/subscribe [::subs/contents-by-position "footer"])]
-   (when @(rf/subscribe [::subs/loading?]) [footer-preview])])
+   (when @(rf/subscribe [::subs/loading?])
+     [:> Typography {:variant "body2"} [:> Skeleton]])])
 
 (defn app-page []
-  [:> Container {:maxWidth "lg"}
-   [:> AppBar {:position "static"}
-    [:> Toolbar [routing/main-nav]]]
-   [:> Grid {:container true :spacing {:xs 2}}
-    [:> Grid {:item true :xs 1} [image]]
-    [:> Grid {:item true :xs 11} [header]]
-    [:> Grid {:item true :xs 1} [side]]
-    [:> Grid {:item true :xs 11} [main]]
-    [:> Grid {:item true :xs 12} [footer]]]])
+  [:> Box {:sx
+           {:bgcolor "grey.300"
+            :minHeight "100vh"}}
+   [:> Container {:maxWidth "lg"}
+    [:> AppBar {:color "transparent"
+                :elevation 0
+                :position "static"}
+     [:> Toolbar [routing/main-nav]]]
+    [:> Grid {:container true :spacing {:xs 2}}
+     [:> Grid {:item true :xs 1} [image]]
+     [:> Grid {:item true :xs 11} [header]]
+     [:> Grid {:item true :xs 1} [side]]
+     [:> Grid {:item true :xs 11} [main]]
+     [:> Grid {:item true :xs 12} [footer]]]]])
 
 (defn app []
   [routing/page-router [app-page]])
